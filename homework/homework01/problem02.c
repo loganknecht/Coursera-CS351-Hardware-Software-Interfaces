f85ddde267f60dee9ee4be1083f5aac30fe072f0
Hugbot <loganknecht@gmail.com>
2016-07-20 23:15:31 -0700

    Problem 02.

--- homework/homework01/problem02.c
+++ homework/homework01/problem02.c
@@ -7,15 +7,17 @@
 // }
 
 // "Fixed"
-// int test(int i, long int l) {
-//     return i == ((int)l);
-// }
+int test(int i, long int l) {
+    return i == ((int)l);
+}
 
 int main(int argc, const char* argv[]) {
-    // int i = rand() % 20;
-    // // creates two random 2 byte ints and assigns them to a 4 byte long int
-    // long int l = (rand() << 32) | rand();
-    // test(i, l);
+    int i = rand() % 20;
+    // creates two random 2 byte ints and assigns them to a 4 byte long int
+    long int l = (rand() << 31) | rand();
+    int test_output = test(i, l);
+    printf("test_output: %d", test_output);
+    printf("\n");
     
     return 0;
 }

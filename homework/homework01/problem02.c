d6f6b0c474fc39b2c043afb3e4d13eb4c7fc0cb2
Hugbot <loganknecht@gmail.com>
2016-07-20 23:03:21 -0700

    Added homework C file.

---
+++ homework/homework01/problem02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Original
+// int test(int i, long int l) {
+//     return i == ((int)l);
+// }
+
+// "Fixed"
+int test(int i, long int l) {
+    return i == ((int)l);
+}
+
+int main(int argc, const char* argv[]) {
+    int i = rand() % 20;
+    // creates two random 2 byte ints and assigns them to a 4 byte long int
+    long int l = (rand() << 32) | rand();
+    test(i, l);
+}

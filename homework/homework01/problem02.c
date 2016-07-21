2385fc271914e14385a5bfa8a1432d6be2e97159
Hugbot <loganknecht@gmail.com>
2016-07-20 23:22:10 -0700

    Fix random numbers for problem02.c

--- homework/homework01/problem02.c
+++ homework/homework01/problem02.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // Original
 // int test(int i, long int l) {
@@ -12,10 +13,13 @@ int test(int i, long int l) {
 }
 
 int main(int argc, const char* argv[]) {
-    int i = rand() % 20;
+    srand(time(NULL));
+    int i = rand();
     // creates two random 2 byte ints and assigns them to a 4 byte long int
     long int l = (rand() << 31) | rand();
     int test_output = test(i, l);
+    printf("i: %d \n", i);
+    printf("l: %ld \n", l);
     printf("test_output: %d", test_output);
     printf("\n");
     

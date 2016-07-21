03fc79275da8393efd0e9c58f88c9d1f61e2a875
Hugbot <loganknecht@gmail.com>
2016-07-20 23:12:38 -0700

    Added makefile.

--- homework/homework01/problem02.c
+++ homework/homework01/problem02.c
@@ -7,13 +7,15 @@
 // }
 
 // "Fixed"
-int test(int i, long int l) {
-    return i == ((int)l);
-}
+// int test(int i, long int l) {
+//     return i == ((int)l);
+// }
 
 int main(int argc, const char* argv[]) {
-    int i = rand() % 20;
-    // creates two random 2 byte ints and assigns them to a 4 byte long int
-    long int l = (rand() << 32) | rand();
-    test(i, l);
+    // int i = rand() % 20;
+    // // creates two random 2 byte ints and assigns them to a 4 byte long int
+    // long int l = (rand() << 32) | rand();
+    // test(i, l);
+    
+    return 0;
 }
